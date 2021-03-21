a964ed183b98cf20f9811bbc8320250a16622ee7
BlasQu <damianhs138@gmail.com>
2021-03-21 20:03:36 +0100

    EmailValidator util added and viewinding for MainActivity

--- app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
+++ app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.unittestingtutorial.feature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.unittestingtutorial.R
+import com.example.unittestingtutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/example/unittestingtutorial/util/EmailValidator.kt
@@ -0,0 +1,17 @@
+package com.example.unittestingtutorial.util
+
+import java.util.regex.Pattern
+
+class EmailValidator {
+    private val EMAIL_PATTERN = Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+")
+    fun isValid(email: String): Boolean {
+        return EMAIL_PATTERN.matcher(email).matches()
+    }
+}
