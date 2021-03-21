321caf244fc4794d979d766e3d9e45877a2a8d06
BlasQu <damianhs138@gmail.com>
2021-03-21 22:20:31 +0100

    Unit tests for email validator

---
+++ app/src/androidTest/java/com/example/unittestingtutorial/EmailValidatorTest.kt
@@ -0,0 +1,41 @@
+package com.example.unittestingtutorial
+
+import com.example.unittestingtutorial.util.EmailValidator
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+class EmailValidatorTest {
+    @Test
+    fun `emailValidator_correctEmail_true`() {
+        assertTrue(EmailValidator().isValid("test@gmail.com"))
+    }
+    @Test
+    fun `emailValidator_correctEmailSubDomain_true`() {
+        assertTrue(EmailValidator().isValid("test@gmail.co.uk"))
+    }
+    @Test
+    fun `emailValidator_blankEmail_false`() {
+        assertFalse(EmailValidator().isValid(""))
+    }
+    @Test
+    fun `emailValidator_EmailSubsDomains_true`() {
+        assertTrue(EmailValidator().isValid("test@gmail.co.me.uk"))
+    }
+    @Test
+    fun `emailValidator_emailWithoutSign_false`() {
+        assertFalse(EmailValidator().isValid("test.com"))
+    }
+    @Test
+    fun `emailValidator_emailWithoutDots_false`() {
+        assertFalse(EmailValidator().isValid("test@gmail"))
+    }
+    @Test
+    fun `emailValidator_emailWithDoubleSigns_false`() {
+        assertFalse(EmailValidator().isValid("test@@gmail.com"))
+    }
+    @Test
+    fun `emailValidator_emailWithDoubleDots_false`() {
+        assertFalse(EmailValidator().isValid("test@gmail..com"))
+    }
+}
