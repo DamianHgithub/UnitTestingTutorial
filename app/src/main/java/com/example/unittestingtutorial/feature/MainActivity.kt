8f6dbf2b662dd737dba2702c427a86e2f0a7d13f
BlasQu <damianhs138@gmail.com>
2021-03-21 21:39:40 +0100

    Creating logic and ui listeners for mainactivity

--- app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
+++ app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.unittestingtutorial.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.unittestingtutorial.R
 import com.example.unittestingtutorial.databinding.ActivityMainBinding
+import com.example.unittestingtutorial.util.EmailValidator
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnCheckemail.setOnClickListener {
+            if (EmailValidator().isValid(binding.inputEmail.text.toString()))
+                Snackbar.make(binding.root, "Email is valid.", Snackbar.LENGTH_SHORT).show()
+            else binding.inputEmail.error = "Email is not valid."
+        }
     }
 }
