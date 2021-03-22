ed94c93f8eb4fcdd099d114918b68b4ebd0f3995
BlasQu <damianhs138@gmail.com>
2021-03-22 01:03:07 +0100

    Creating datastore to pass/store email

--- app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
+++ app/src/main/java/com/example/unittestingtutorial/feature/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.unittestingtutorial.feature
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.unittestingtutorial.R
 import com.example.unittestingtutorial.databinding.ActivityMainBinding
 import com.example.unittestingtutorial.util.EmailValidator
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    val datastore: DataStore<Preferences> by preferencesDataStore("EmailDatastore")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,5 +30,15 @@ class MainActivity : AppCompatActivity() {
                 Snackbar.make(binding.root, "Email is valid.", Snackbar.LENGTH_SHORT).show()
             else binding.inputEmail.error = "Email is not valid."
         }
+    }
+    private suspend fun writeToDatastore(email: String) {
+        val key = stringPreferencesKey("LastEmail")
+        datastore.edit {
+            it[key] = email
+        }
+    }
+    private suspend fun getFromDatastore(email: String): String? {
+        val key = stringPreferencesKey("LastEmail")
+        return datastore.data.first()[key]
     }
 }
