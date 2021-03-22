0b47e18769b005e9b252495bc34e7bdc6f2e1e1e
BlasQu <damianhs138@gmail.com>
2021-03-22 17:40:49 +0100

    Viewmodel and repository setup

--- app/src/main/java/com/example/unittestingtutorial/di/Modules.kt
+++ app/src/main/java/com/example/unittestingtutorial/di/Modules.kt
@@ -1,2 +1,11 @@
 package com.example.unittestingtutorial.di
 
+import com.example.unittestingtutorial.feature.MainRepository
+import com.example.unittestingtutorial.feature.MainViewmodel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val architectureModule = module {
+    single { MainRepository() }
+    viewModel { MainViewmodel(get()) }
+}
---
+++ app/src/main/java/com/example/unittestingtutorial/feature/MainViewmodel.kt
@@ -0,0 +1,8 @@
+package com.example.unittestingtutorial.feature
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewmodel(
+    val repository: MainRepository
+): ViewModel() {
+}
